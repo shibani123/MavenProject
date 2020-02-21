5a39757ed0edad50cea824cff50aa11bd9aeac86
Shibani Bhandary <shibani.bhandary@aciworldwide.com>
2020-02-21 10:39:44 +0530

    First Maven Commit

---
+++ src/main/java/com/aaciwebapp/aciwebapp/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.aaciwebapp.aciwebapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home")
+	public String ViewHomePage() {
+		return "home";
+	}
+}
---
+++ src/test/java/com/aaciwebapp/aciwebapp/AciwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaciwebapp.aciwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AciwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
